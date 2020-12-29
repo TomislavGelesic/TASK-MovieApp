f4d70072b37b1ff92121f65c55b7ecad37ab49e0
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2020-12-29 18:20:02 +0100

    CoordinatorType enumeration

---
+++ TASK-MovieApp/Common/CommonModels/CoordinatorType.swift
@@ -0,0 +1,15 @@
+//
+//  CoordinatorType.swift
+//  TASK-MovieApp
+//
+//  Created by Tomislav Gelesic on 29.12.2020..
+//
+
+import Foundation
+
+enum CoordinatorType {
+    case FavouriteScene
+    case WatchedScene
+    case NowPlayingScene
+    case DetailScene
+}
